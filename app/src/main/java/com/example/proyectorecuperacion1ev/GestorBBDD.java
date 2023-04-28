b6981ec213d07743372552047ee7a8eb28dfa8df
jaimesanfelix <jaimesanfelix7@gmail.com>
2023-04-28 13:46:29 +0200

    Initial commit

---
+++ app/src/main/java/com/example/proyectorecuperacion1ev/GestorBBDD.java
@@ -0,0 +1,31 @@
+package com.example.proyectorecuperacion1ev;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+// Esta clase es la que maneja la BBDD. La podríamos reusar para cualquier proyecto de bbdd con SQLite
+public class GestorBBDD extends SQLiteOpenHelper {
+
+    private String sqlCreacion;
+
+    // El constructor simplemente llama al constructor padre. Podemos generarlo automáticamente con el menú "Generate-Constructor"
+    public GestorBBDD(@Nullable Context context, @Nullable String name, int version, String sqlCreacion) {
+        super(context, name, null, version); // Le pasamos null en el parámetro SQLiteDatabase.CursorFactory para que use uno por defecto
+        this.sqlCreacion = sqlCreacion;
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("CREATE TABLE IF NOT EXISTS Comarques (nom TEXT, provincia TEXT, capital TEXT, poblacio INTEGER, latitud FLOAT, longitud FLOAT, url TEXT)");
+        db.execSQL(sqlCreacion);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+        // Cuando cambiamos de versión, modificaciones a hacer en el esquema de BBDD
+        // No lo implementaremos
+    }
+}
