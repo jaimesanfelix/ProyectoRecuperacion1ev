fca3c054471747c693a4c307c410403b868731b2
jaimesanfelix <jaimesanfelix7@gmail.com>
2023-05-15 18:21:12 +0200

    Sesion 15/05/23

--- app/src/main/java/com/example/proyectorecuperacion1ev/GestorBBDD.java
+++ app/src/main/java/com/example/proyectorecuperacion1ev/GestorBBDD.java
@@ -9,22 +9,27 @@ import androidx.annotation.Nullable;
 // Esta clase es la que maneja la BBDD. La podríamos reusar para cualquier proyecto de bbdd con SQLite
 public class GestorBBDD extends SQLiteOpenHelper {
 
-    private String sqlCreacion;
+   // private String sqlCreacion;
+    public static final String NOMBRE_BBDD = "comarcas";
 
     // El constructor simplemente llama al constructor padre. Podemos generarlo automáticamente con el menú "Generate-Constructor"
     public GestorBBDD(@Nullable Context context, @Nullable String name, int version, String sqlCreacion) {
         super(context, name, null, version); // Le pasamos null en el parámetro SQLiteDatabase.CursorFactory para que use uno por defecto
-        this.sqlCreacion = sqlCreacion;
+       // this.sqlCreacion = sqlCreacion;
+    }
+
+    public GestorBBDD(@Nullable Context context, int version){
+        super(context, NOMBRE_BBDD, null, version);
     }
 
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL("CREATE TABLE IF NOT EXISTS Comarques (nom TEXT, provincia TEXT, capital TEXT, poblacio INTEGER, latitud FLOAT, longitud FLOAT, url TEXT)");
-        db.execSQL(sqlCreacion);
+      //  db.execSQL(sqlCreacion);
     }
 
     @Override
-    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
         // Cuando cambiamos de versión, modificaciones a hacer en el esquema de BBDD
         // No lo implementaremos
     }
--- app/src/main/java/com/example/proyectorecuperacion1ev/MainActivity.java
+++ app/src/main/java/com/example/proyectorecuperacion1ev/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.proyectorecuperacion1ev;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.StrictMode;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.SpinnerAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.BufferedReader;
@@ -30,31 +43,191 @@ public class MainActivity extends AppCompatActivity {
     private Spinner spComarcas;
     private static final String COMARCA = "com.example.proyectoRecuperacion.MainActivity.comarca";
     private String comarcaPreferida = null;
+    private SQLiteDatabase db;
+    private Button btRepoblarBBDD;
+    private Button btBorrar;
+    private TextView tvProvincia;
+    private TextView tvCapital;
+    private TextView tvPoblacion;
+    private TextView tvLatitud;
+    private TextView tvLongitud;
+    private LocationManager locationManager = null;
+    private LocationListener locationListener;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         spComarcas = findViewById(R.id.sp_comarca);
-        cargarSpinnerComarcas();
-        cargarPreferencias();
-        if(comarcaPreferida != null){
-            seleccionarComarca(comarcaPreferida);
+        btRepoblarBBDD = findViewById(R.id.bt_repoblar);
+        btRepoblarBBDD.setOnClickListener(this::onRepoblarBBDD);
+        btBorrar = findViewById(R.id.bt_borrar);
+        btBorrar.setOnClickListener(this::onBorrar);
+        tvProvincia = findViewById(R.id.tv_provincia);
+        tvCapital = findViewById(R.id.tv_capital);
+        tvPoblacion = findViewById(R.id.tv_poblacion);
+        tvLatitud = findViewById(R.id.tv_latitud);
+        tvLongitud = findViewById(R.id.tv_longitud);
+        configurarSpinnerComarcasListener();
+        iniciarLocalizacion();
+    }
+
+    private void iniciarLocalizacion() {
+
+        configurarPoliticaThreads();
+        pedirPermisosLocalizacion();
+        inicializarLocationManager();
+        inicializarInfoGps();
+        inicializarLocationListener();
+
+    }
+
+    private void inicializarLocationManager() {
+
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+    }
+
+    private void inicializarInfoGps() {
+
+        Location localizacion = obtenerUltimaUbicacionConocida();
+        actualizarInfoGps(localizacion);
+
+    }
+
+    private Location obtenerUltimaUbicacionConocida() {
+
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            System.out.println("No se a obtenido los permisos necesarios");
+            return null;
         }
-        getComandosInsertRepoblarBBDD();
+        Location localizacion = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+
+
+        return localizacion;
+    }
+
+    private void actualizarInfoGps(Location localizacion) {
+
+        if (localizacion == null) {
+            System.out.println("Localizacion null");
+            return;
+        }
+
+        double latitud = localizacion.getLatitude();
+        double longitud = localizacion.getLongitude();
+        actualizarIULocalizacionGps(latitud, longitud);
+
+    }
+
+    private void actualizarIULocalizacionGps(double latitud, double longitud) {
+
+        tvLatitud.setText(String.format("%.4f", latitud));
+        tvLongitud.setText(String.format("%.4f", longitud));
+
+    }
+
+    private void inicializarLocationListener() {
+
+        locationListener = new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+
+                actualizarInfoGps(location);
+
+            }
+        };
+
+    }
+
+
+    private void pedirPermisosLocalizacion() {
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION, android.Manifest.permission.ACCESS_FINE_LOCATION}, 0);
+
+        }
+
+    }
+
+
+    private void configurarPoliticaThreads() {
+
+        StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+        StrictMode.setThreadPolicy(policy);
+
+    }
+
+    private void encenderListenerLocalizacion() {
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 1, locationListener);
+
+    }
+
+    private  void apagarListenerLocalizacion(){
+
+        locationManager.removeUpdates(locationListener);
+
     }
 
     @Override
     protected void onPause() {
+        db.close();
         guardarPreferencias();
+        apagarListenerLocalizacion();
+        System.out.println("Estoy en el on pause");
         super.onPause();
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        System.out.println("Estoy en el on resume");
+        db = new GestorBBDD(this, 1).getWritableDatabase();
+       cargarSpinnerComarcas();
+        cargarPreferencias();
+        if(comarcaPreferida != null){
+            seleccionarComarca(comarcaPreferida);
+        }
+        encenderListenerLocalizacion();
+
     }
 
     public void onNavegar(View view){
 
-       navegar("https://google.com//");
+       String comarca = getComarcaSeleccionada();
+       navegarComarca(comarca);
 
     }
+
+
+    private void navegarComarca(String comarca){
+
+        String comarcaUrl = "https://" + getComarcaUrl(comarca);
+        navegar(comarcaUrl);
+
+    }
+
+
 
     private void navegar(String url) {
 
@@ -65,6 +238,24 @@ public class MainActivity extends AppCompatActivity {
         abrirWeb.setData(Uri.parse(url));
 
         this.startActivity(abrirWeb);
+
+    }
+
+    private String getComarcaUrl(String comarca){
+
+        String consultaComarca = "SELECT url FROM Comarques WHERE nom = ?";
+
+        String[] argumentos = {comarca};
+
+        Cursor cursor = db.rawQuery(consultaComarca, argumentos);
+
+        if(cursor.getCount() > 0){
+            cursor.moveToFirst();
+            String comarcaUrl = cursor.getString(0);
+            cursor.close();
+            return comarcaUrl;
+        }
+        return null;
 
     }
 
@@ -87,21 +278,55 @@ public class MainActivity extends AppCompatActivity {
         if(requestCode == REQUEST_CODE){
             if(resultCode == RESULT_OK){
                 provinciaFiltro = data.getIntExtra(SeleccionProvinciaActivity.PROVINCIA, SeleccionProvinciaActivity.TODAS);
-                cargarSpinnerComarcas();
-                if(comarcaPreferida != null){
-                    seleccionarComarca(comarcaPreferida);
-                }
             }
         }
     }
 
+    private void onBorrar(View view){
+
+        String comarcaSeleccionada = getComarcaSeleccionada();
+
+        borrarComarca(comarcaSeleccionada);
+        cargarSpinnerComarcas();
+        Toast.makeText(this, "La comarca " + comarcaSeleccionada + " a sido eliminada con exito", Toast.LENGTH_SHORT).show();
+
+
+    }
+
+    private void borrarComarca(String comarca){
+
+        String consultaComarca = "DELETE FROM Comarques WHERE nom = ?";
+
+        Object[] argumentos = {comarca};
+
+        db.execSQL(consultaComarca, argumentos);
+
+    }
+
     private String[] getComarcas(String filtroProvincia){
 
-        String consulta = "SELECT nom FROM Comarques WHERE Provincia = ? OR ? IS NULL";
+        String consulta = "SELECT nom FROM Comarques WHERE Provincia = ? OR ? IS NULL AND Provincia = Provincia";
         String[] argumentos = filtroProvincia != null && !filtroProvincia.isEmpty() ? new String[] {filtroProvincia, filtroProvincia} : null;
-        return new String[] {
-                "Comarca1", "Comarca2", "Comarca3", "Comarca4"
-        };
+        if(db == null){
+            return null;
+        }
+        Cursor cur = db.rawQuery(consulta, argumentos);
+
+        List<String> comarcas = null;
+
+        if(cur.moveToFirst()){
+
+            comarcas = new ArrayList<>();
+            do {
+
+                String nombreComarca = cur.getString(0);
+                comarcas.add(nombreComarca);
+
+            }while (cur.moveToNext());
+
+        }
+        cur.close();
+        return comarcas == null ? null:comarcas.toArray(new String[0]);
     }
 
     private SpinnerAdapter crearSpinnerComarcasAdapter(){
@@ -109,6 +334,9 @@ public class MainActivity extends AppCompatActivity {
         String filtro = getFiltroFromSelectedProvincia(provinciaFiltro);
 
         String[] comarcas = getComarcas(filtro);
+        if (comarcas == null) {
+            return null;
+        }
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, comarcas);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         return adapter;
@@ -161,7 +389,25 @@ public class MainActivity extends AppCompatActivity {
 
     private void mostrarInfoComarca(String comarca) {
 
-        //TO-DO Se completara cuando empecemos con las bases de datos
+        String consultaComarca = "SELECT * FROM Comarques WHERE nom = ?";
+
+        String[] argumentos = {comarca};
+
+        Cursor cursor = db.rawQuery(consultaComarca, argumentos);
+
+        if(cursor.getCount() > 0){
+            cursor.moveToFirst();
+            String provincia = cursor.getString(1);
+            String capital = cursor.getString(2);
+            int poblacion = cursor.getInt(3);
+            float latitud = cursor.getFloat(4);
+            float longitud = cursor.getFloat(5);
+            cursor.close();
+            tvProvincia.setText(provincia);
+            tvCapital.setText(capital);
+            tvPoblacion.setText(Integer.toString(poblacion));
+        }
+
 
     }
 
@@ -193,11 +439,20 @@ public class MainActivity extends AppCompatActivity {
 
     private void seleccionarComarca(String comarca){
 
+        if(comarca == null){
+            return;
+        }
+
         ArrayAdapter<String> adaptador = (ArrayAdapter<String>) spComarcas.getAdapter();
+
+        if(adaptador == null){
+            return;
+        }
 
         int posicion = adaptador.getPosition(comarca);
 
         spComarcas.setSelection(posicion);
+
 
     }
 
@@ -219,6 +474,25 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    private void onRepoblarBBDD(View view){
+
+        repoblarBBDD();
+        cargarSpinnerComarcas();
+        seleccionarComarca(comarcaPreferida);
+
+    }
+
+    private void repoblarBBDD(){
+
+        db.execSQL("DELETE FROM Comarques");
+        for (String comandoInsert: getComandosInsertRepoblarBBDD()) {
+            db.execSQL(comandoInsert);
+        }
+        
+    }
+
+
 
     private List<String> getComandosInsertRepoblarBBDD(){
 
